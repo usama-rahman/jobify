cdc9ac9f6337cfeb845c6fba7a6a43c17e357bb8
usama-rahman <rahmanusama390@gmail.com>
2024-08-21 08:07:15 +0600

    Dark Mode Added

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function JobPage() {
   return <h1 className="text-4xl">JobsPage </h1>;
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,11 +6,9 @@ import { PropsWithChildren } from "react";
 function layout({ children }: PropsWithChildren) {
   return (
     <main className="grid lg:grid-cols-5">
-      {/* first-col hide on small screen */}
       <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
         <Sidebar />
       </div>
-      {/* second-col hide dropdown on big screen */}
 
       <div className="lg:col-span-4">
         <Navbar />
