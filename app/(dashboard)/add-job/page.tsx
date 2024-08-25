749c778ff67619f45674be63e6d90602a478ff89
usama-rahman <rahmanusama390@gmail.com>
2024-08-25 09:31:29 +0600

    React Query

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,9 +1,17 @@
 import CreateJobForm from "@/components/CreateJobForm";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-export default function AddJobPage() {
+function AddJobPage() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <CreateJobForm />
-    </>
+    </HydrationBoundary>
   );
 }
+
+export default AddJobPage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -4,8 +4,6 @@ import prisma from "./db";
 import { auth } from "@clerk/nextjs";
 import { JobType, CreateAndEditJobType, createAndEditJobSchema } from "./types";
 import { redirect } from "next/navigation";
-import { Prisma } from "@prisma/client";
-import dayjs from "dayjs";
 
 function authenticateAndRedirect(): string {
   const { userId } = auth();
@@ -18,7 +16,6 @@ function authenticateAndRedirect(): string {
 export async function createJobAction(
   values: CreateAndEditJobType
 ): Promise<JobType | null> {
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
   const userId = authenticateAndRedirect();
   try {
     createAndEditJobSchema.parse(values);
